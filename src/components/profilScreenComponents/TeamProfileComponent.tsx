e63de713ba338680d63773d339831c41cadabca6
johan030 <weylin030@protonmail.ch>
2022-06-02 11:00:18 +0200

    fix perfect circles around text on teamComponent

--- src/components/profilScreenComponents/TeamProfileComponent.tsx
+++ src/components/profilScreenComponents/TeamProfileComponent.tsx
@@ -20,26 +20,27 @@ const TeamProfileComponent = () => {
       </View>
 
       <View style = {styles.inlineContent}>
-        <TouchableOpacity>
+        <TouchableOpacity style = {styles.circle_blue}>
           <Text>Famille</Text>
           <Text style = {{textAlign: "center"}}>12</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity style = {styles.circle_brown}>
           <Text >Amis Proche</Text>
           <Text style = {{textAlign: "center"}}>4</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity style = {styles.circle_green}>
           <Text>Amis</Text>
           <Text style = {{textAlign: "center"}}>16</Text>
         </TouchableOpacity>
-        <TouchableOpacity >
-          <Text>Collègues</Text>
+        <TouchableOpacity style = {styles.circle_red} >
+          <Text>Collègue</Text>
           <Text style = {{textAlign: "center"}}>26</Text>
         </TouchableOpacity>
       </View>
     </>
   );
 };
+
 
 const styles = StyleSheet.create({
   inlineContent: {
@@ -50,15 +51,44 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
 
-  circle:{
-    width: 50,
-    height: 50,
-    lineHeight: 50,
-    borderRadius: "50",
+  circle_red:{
+    width: 70,
+    height: 70,    
+    borderWidth: 7,
+    borderRadius: 50,
     fontSize: 50,
-    color: "#fff",
-    textAlign: "center",
-    
+    borderColor: "red",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  circle_green:{
+    width: 70,
+    height: 70,
+    borderWidth: 7,
+    borderRadius: 50,
+    borderColor: "green",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  circle_brown:{
+    width: 70,
+    height: 70,
+    borderWidth: 7,
+    borderRadius: 50,
+    fontSize: 50,
+    borderColor: "brown",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  circle_blue:{
+    width: 70,
+    height: 70,
+    borderWidth: 7,
+    borderRadius: 50,
+    fontSize: 29,
+    borderColor: "blue",
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 
   
