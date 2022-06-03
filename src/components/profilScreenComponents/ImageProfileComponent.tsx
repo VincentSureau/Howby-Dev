fc667d94a70b24d27ec21806ccbc1842af110c17
vincent sureau <vincentsureau5@gmail.com>
2022-06-03 18:04:06 +0200

    hotfix img uri

--- src/components/profilScreenComponents/ImageProfileComponent.tsx
+++ src/components/profilScreenComponents/ImageProfileComponent.tsx
@@ -1,6 +1,6 @@
 import { View, StyleSheet, Platform } from 'react-native'
 import React from 'react'
-import { Image } from 'react-native-ui-lib'
+import { Image, Text } from 'react-native-ui-lib'
 import { LinearGradient } from 'expo-linear-gradient';
 
 const img = {uri: "https://geo.img.pmdstatic.net/fit/https.3A.2F.2Fi.2Epmdstatic.2Enet.2Fgeo.2F2022.2F01.2F06.2Fdc9e1cb3-9288-40dc-ab66-0947f59d9b42.2Ejpeg/1280x720/background-color/ffffff/quality/70/la-montagne-face-au-changement-climatique-ce-que-lon-sait-et-comment-on-sadapte.jpg"};
