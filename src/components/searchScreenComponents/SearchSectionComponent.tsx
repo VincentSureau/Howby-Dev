93c85ad63d42c1d1a0338cd9902198e088c7cb79
johan030 <weylin030@protonmail.ch>
2022-06-14 15:41:01 +0200

    cleanup

--- src/components/searchScreenComponents/SearchSectionComponent.tsx
+++ src/components/searchScreenComponents/SearchSectionComponent.tsx
@@ -8,35 +8,49 @@ const SearchSectionComponent = () => {
       <View style={styles.container}>
         <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
           <View>
-            <TouchableOpacity style = {{backgroundColor: 'gray', paddingTop: 10, paddingBottom: 10, paddingLeft: 5, paddingRight: 60, borderRadius: 5, marginBottom: 20 }}>
-            <Text style = {styles.text}>Recherche</Text>
-            <Ionicons name = 'search-outline' size = {20} style = {{position: 'absolute', bottom: 0, right: 5}}/>
+            <TouchableOpacity style={styles.touchableArea}>
+              <Text style={styles.text}>Recherche</Text>
+              <Ionicons name="search-outline" size={20} style={styles.icon} />
             </TouchableOpacity>
-            <TouchableOpacity style = {{backgroundColor: 'gray', paddingTop: 10, paddingBottom: 10, paddingLeft: 5, paddingRight: 40, borderRadius: 5, marginBottom: 20 }}>
-
-            <Text style ={{textTransform: 'uppercase', fontSize: 18, color: 'brown'}}>Derniere</Text>
-            <Text style ={{textTransform: 'uppercase', fontSize: 18, color: 'brown'}}>Minute</Text>
-            <Ionicons name = 'return-down-back-outline' size = {20} style = {{position: 'absolute', bottom: 0, right: 5}} />
+            <TouchableOpacity style={styles.touchableArea}>
+              <Text style={{textTransform: 'uppercase', fontSize: 18, color: 'brown'}}>
+                Derniere
+              </Text>
+              <Text style={{textTransform: 'uppercase', fontSize: 18, color: 'brown'}}>Minute</Text>
+              <Ionicons name="return-down-back-outline" size={20} style={styles.icon} />
             </TouchableOpacity>
-
-            <Text>Limited Event</Text>
+            <TouchableOpacity style={styles.touchableArea}>
+              <Text style={{textTransform: 'uppercase', fontSize: 18, color: 'brown'}}>
+                Limited
+              </Text>
+              <Text style={{textTransform: 'uppercase', fontSize: 18, color: 'brown'}}>Event</Text>
+              <Ionicons name="diamond-outline" size={20} style={styles.icon} />
+            </TouchableOpacity>
           </View>
 
           <View>
-            <TouchableOpacity style = {{backgroundColor: 'gray', paddingTop: 10, paddingBottom: 10, paddingLeft: 25, paddingRight: 50, borderRadius: 5, marginBottom: 20 }}>
-            <Text style = {styles.text}>Mon Panier</Text>
-            <Ionicons name = 'cart-outline' size= {20}  style = {{position: 'absolute', bottom: 0, right: 5}}/>
+            <TouchableOpacity style={styles.touchableArea}>
+              <Text style={styles.text}>Mon Panier</Text>
+              <Ionicons
+                name="cart-outline"
+                size={20}
+                style={{position: 'absolute', bottom: 0, right: 5}}
+              />
             </TouchableOpacity>
-            <TouchableOpacity  style = {{backgroundColor: 'gray', paddingTop: 10, paddingBottom: 10, paddingLeft: 25, paddingRight: 50, borderRadius: 5, marginBottom: 20 }}>
-            <Text style ={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>Nouvelle</Text>
-            <Text  style ={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>Organisation</Text>
+            <TouchableOpacity style={styles.touchableArea}>
+              <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>Nouvelle</Text>
+              <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>
+                Organisation
+              </Text>
             </TouchableOpacity>
-            <TouchableOpacity style = {{backgroundColor: 'gray', paddingTop: 10, paddingBottom: 10, paddingLeft: 25, paddingRight: 50, borderRadius: 5, marginBottom: 20}}>
-                <Text style ={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>Organisation</Text>
-                <Text style ={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>en cours</Text>
+            <TouchableOpacity style={styles.touchableArea}>
+              <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>
+                Organisation
+              </Text>
+              <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>en cours</Text>
             </TouchableOpacity>
-            <TouchableOpacity style = {{backgroundColor: 'gray', paddingTop: 10, paddingBottom: 10, paddingLeft: 25, paddingRight: 50, borderRadius: 5, marginBottom: 20}}>
-            <Text  style ={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>Favoris</Text>
+            <TouchableOpacity style={styles.touchableArea}>
+              <Text style={{textAlign: 'center', fontWeight: 'bold', fontSize: 16}}>Favoris</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -55,7 +69,20 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     color: '#001C33',
-   
+  },
+  icon: {
+    position: 'absolute',
+    bottom: 0,
+    right: 5,
+  },
+  touchableArea: {
+    backgroundColor: 'gray',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 25,
+    paddingRight: 50,
+    borderRadius: 5,
+    marginBottom: 20,
   },
 });
 export default SearchSectionComponent;
