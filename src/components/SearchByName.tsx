974f6ba1fbbaabfd6a10d4893d233e864231261d
vincent sureau <vincentsureau5@gmail.com>
2022-06-16 16:29:05 +0200

    hotfix: refacto + fix of search input component

--- src/components/SearchByName.tsx
+++ src/components/SearchByName.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { View, Text, TextInput } from 'react-native'
+import React, { useCallback, useEffect } from 'react'
+import { TextInput, Alert, FlatList } from 'react-native'
 import { useRef, useState } from 'react'
-import { FlatList } from 'react-native-gesture-handler'
 import { isNotEmptyArray } from '@formiz/validations'
+import { Text, View } from 'react-native-ui-lib'
 
 const data = [
     {id:1, name : 'johan'},
@@ -12,36 +12,75 @@ const data = [
     {id:5, name : 'Léo'},
     {id:6, name : 'Hayley'}
 ]
-const SearchByName = () => {
-    const [dataFromState, setData] = useState(data)
 
-    const item = ({item}) => {
+export type ContactType = {
+    id: number;
+    name: string;
+}
+
+interface SearchByNameProps {
+    contacts: Array<ContactType>;
+}
+
+const SearchByName = ({contacts}: SearchByNameProps) => {
+    // originalUsers will store the complete list of the user contacts
+    const [originalUsers, setOriginalUsers] = useState<Array<ContactType>>([]);
+    // filteredUsers will store the filtered list of the user contacts
+    // we need 2 states because if we only use on, the filtered user will disapears
+    // and won't be found if the user remove of reinitialize the search input
+    const [filteredUsers, setFilteredUsers] = useState<Array<ContactType>>([]);
+
+    // when the component is displayed, we retrieve the user contact list
+    const start = useCallback(async () => {
+        try {
+            // todo: retrieve contact list by API when ready
+            // const data = await api.user.getContacts();
+            setOriginalUsers(data);
+            setFilteredUsers(data);
+        } catch (e) {
+          Alert.alert('Error', 'There was a problem fetching data :(');
+        }
+      }, [/*api.counter*/]);
+    
+      useEffect(() => {
+        start();
+      }, []);
+          
+
+    const renderItem = ({item}: {item: ContactType}) => {
         return(
             <View>
                 <Text style = {{fontSize: 30}}>{item.name}</Text>
             </View>
         )
     }
+
     const searchName = (input: string) => {
-        const data  = dataFromState;
-        const searchData = data.filter((item) => {
-            return item.name;
+        const searchData = originalUsers.filter((item) => {
+            // we search contact name that includes the input in their name
+            // we trim and lowercase the input and the original name
+            return item.name.toLowerCase().includes(input.trim().toLowerCase());
         });
-        setData(searchData)
+        // we change the value of filteredUsers
+        // at every change of this value, the flatlist will be updated
+        setFilteredUsers(searchData);
     }
   return (
     <View>
         <View>
             <TextInput 
-            placeholder = "Search Name"
-            onChangeText={(input)=>{
-                searchName(input)
-            }} />
+                placeholder = "Search Name"
+                onChangeText={(input)=>{
+                    searchName(input)
+                }} 
+            />
         </View>
 
-      <FlatList data = {dataFromState}
-      renderItem = {item}
-      keyExtractor = {(item, index) => index.toString()} />
+        <FlatList 
+            data={filteredUsers}
+            renderItem={renderItem}
+            keyExtractor = {item => item.id.toString()} 
+        />
     </View>
   )
 }
