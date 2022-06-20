350c7220c10206fed6e5fca6421b1cefcaebbf0b
johan030 <weylin030@protonmail.ch>
2022-06-20 15:03:15 +0200

    adding sticky stories facebooklike component

--- src/components/userFeedComponents/Slider.tsx
+++ src/components/userFeedComponents/Slider.tsx
@@ -1,16 +1,16 @@
-import { View, Text, ScrollView, Image, StyleSheet, Alert } from 'react-native'
-import React from 'react'
-import { IMAGES } from '../../data/Images';
+import {View, Text, ScrollView, Image, StyleSheet, Alert} from 'react-native';
+import React from 'react';
+import {IMAGES} from '../../data/Images';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import StickyItemFlatList from '@gorhom/sticky-item';
 import Animated from 'react-native-reanimated';
+import { Icon } from 'react-native-ui-lib';
+
 
 // dummy data
-const data = [...Array(20)]
-  .fill(0)
-  .map((_, index) => ({ id: `item-${index}` }));
+const data = [...Array(20)].fill(0).map((_, index) => ({id: `item-${index}`}));
 
 // configs
 const ITEM_WIDTH = 90;
@@ -31,21 +31,20 @@ const StickyItemView = ({
   separatorSize,
   isRTL,
 }) => {
-
   const amazingAnimation = {
-    // here you add your custom interactive animation
-    // based on the animated value `x`
-  }
 
-  return <Animated.View style={amazingAnimation} />
-}
+    
+  };
+
+  return <Animated.View style={amazingAnimation} />;
+};
 
 const Slider = () => {
   // methods
   const handleStickyItemPress = () => Alert.alert('Sticky Item Pressed');
 
   // render
-  const renderItem = ({ item, index}) => (
+  const renderItem = ({item, index}) => (
     <View
       key={`item-${index}`}
       style={{
@@ -54,23 +53,102 @@ const Slider = () => {
         height: ITEM_HEIGHT,
       }}
     />
-  )
+  );
   return (
-    <StickyItemFlatList
-      itemWidth={ITEM_WIDTH}
-      itemHeight={ITEM_HEIGHT}
-      separatorSize={SEPARATOR_SIZE}
-      borderRadius={BORDER_RADIUS}
-      stickyItemWidth={STICKY_ITEM_WIDTH}
-      stickyItemHeight={STICKY_ITEM_HEIGHT}
-      stickyItemBackgroundColors={STICKY_ITEM_BACKGROUNDS}
-      stickyItemContent={StickyItemView}
-      onStickyItemPress={handleStickyItemPress}
-      data={data}
-      renderItem={renderItem}
-    />
-  )
-}
+    <>
+    <View>
+          <Animated.View style = {[styles.personal_card_container,{
+    
+          }]}
+          >
+            <Animated.View style = {[styles.image_container,{
 
+            }]}
+            >
+              <Image source = {{uri: "https://img.icons8.com/fluency/48/undefined/facebook-new.png"}}  style = {styles.image} />
+            </Animated.View>
+            <Animated.View style = {[styles.cta_container,{
 
-export default Slider
+            }]}
+            >
+              <Animated.Text style = {[styles.text,{
+
+              }]}
+              >
+                Create {"\n"} story
+              </Animated.Text>
+              <Animated.View  style = {[styles.icon_contaier,]}
+              >
+                <Icon source ={{uri: "https://img.icons8.com/fluency/48/undefined/facebook-new.png"}} size= {18} color = {"#ffffff"}/>
+              </Animated.View>
+            </Animated.View>
+          </Animated.View>
+          
+      </View>
+      <StickyItemFlatList
+        itemWidth={ITEM_WIDTH}
+        itemHeight={ITEM_HEIGHT}
+        separatorSize={SEPARATOR_SIZE}
+        borderRadius={BORDER_RADIUS}
+        stickyItemWidth={STICKY_ITEM_WIDTH}
+        stickyItemHeight={STICKY_ITEM_HEIGHT}
+        stickyItemBackgroundColors={STICKY_ITEM_BACKGROUNDS}
+        stickyItemContent={StickyItemView}
+        onStickyItemPress={handleStickyItemPress}
+        data={data}
+        renderItem={renderItem}
+      />
+      
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  personal_card_container: {
+    backgroundColor: '#fff',
+    position: 'absolute',
+    zIndex: 10,
+    elevation: 10,
+    borderTopRightRadius: 16,
+    borderBottomRightRadius: 16,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: '#cccccc',
+  },
+
+  image_container:{
+    position: 'relative',
+    overflow: 'hidden'
+  },
+
+  image: {
+    flex: 1,
+    width: 0,
+    height: 0,
+  },
+
+  cta_container: {
+    position: 'relative',
+
+  },
+
+  text: {
+    textAlign: 'center',
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  icon_contaier: {
+    width: 32,
+    height: 32,
+    borderRadius: 32,
+    backgroundColor: '#3578e5',
+    position: 'absolute',
+    borderWidth: 3,
+    borderColor: '#FFF',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default Slider;
